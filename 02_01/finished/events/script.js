69f6b0878e95f2f1b6964dc74831174895df7ea2
Digbijay-Nayak <nayakdigbijay888@gmail.com>
2023-12-06 16:38:02 +0530

    initial commit

---
+++ 02_01/finished/events/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.grid').addEventListener(
+  'click',
+  function (e) {
+    var removeTarget = e.target.parentNode;
+    removeTarget.parentNode.removeChild(removeTarget);
+  },
+  false
+);
